3c73304089ec6df5a1bbda35965d94e55a7930aa
kudou_kyohei <kudou.pg1010@gmail.com>
2019-11-20 18:34:52 +0900

    add action update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    user = User.find(current_user.id)
+    user.update(user_params)
+  end
 
   private
   def user_params
