9beb89079fcc69e4d8e681adce54262607bbbcc6
kudou_kyohei <kudou.pg1010@gmail.com>
2019-11-22 17:38:42 +0900

    create set_group method to message-controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,17 @@
 class MessagesController < ApplicationController
+  before_action :set_group, only: [:index, :create]
 
   def index
-    @messages = Message.all
+    # @messages = Message.
+    # @message = Message.new
   end
 
   def create
+  end
+
+  private
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 
 end
