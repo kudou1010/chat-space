ab22a685e57008cd8d2a4faa1abbf674d7c574f1
kudou_kyohei <kudou.pg1010@gmail.com>
2019-11-25 17:33:31 +0900

    add let instance

--- spec/controllers/message__controller_spec.rb
+++ spec/controllers/message__controller_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 describe MessageController do
+
+  let(:group) { create(:group) }
+  let(:user) { create(:user) }
   
   describe "#index" do
     context 'log in' do
     end
     
     context 'not log in' do
+
     end
   end
 end
