7b8a6262ccebf139201644e1f134bd0cb1ec16a3
kudou_kyohei <kudou.pg1010@gmail.com>
2019-11-27 21:57:42 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,19 +7,20 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Message.new(message_params)
-    resopond_to do |format|
+    @message = Message.create(message_params)
+ 
+    respond_to do |format|
       format.html {  redirect_to group_messages_path, notice: "メッセージを送信しました"}
       format.json
     end
 
-    if @message.save
-      redirect_to group_messages_path(@group), notice: "メッセージを送信しました"
-    else
-      @messages = @group.messages.includes(:user)
-      flash.now[:alert] = "メッセージを入力してください。"
-      render :index
-    end
+    # if @message.save
+    #   redirect_to group_messages_path(@group), notice: "メッセージを送信しました"
+    # else
+    #   @messages = @group.messages.includes(:user)
+    #   flash.now[:alert] = "メッセージを入力してください。"
+    #   render :index
+    # end
   end
 
   private
