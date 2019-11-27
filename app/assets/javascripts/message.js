d49cb7a7e77e3fc4ecad0edb5c486539bec56a19
kudou_kyohei <kudou.pg1010@gmail.com>
2019-11-27 21:57:56 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,70 @@
 $(function(){
+  function buildHTML(message) {
+    // if(message.image) {
+    //   var html = `<div class="main__messages__message">
+    //                 <div class="main__messages__message__heading">
+    //                   <div class="main__messages__message__heading__name">
+    //                     ${message.name}
+    //                   </div>
+    //                   <div class="main__messages__message__heading__date">
+    //                     ${message.created_at}
+    //                   </div>
+    //                 </div>
+    //                 <div class="main__messages__message__content"></div>
+    //                   <div class="main__messages__message__text">
+    //                     ${message.body}
+    //                   </div>
+    //                   <div class="main__messages__message__image">
+    //                     ${message.image}
+    //                   </div>
+    //                 </div>`
+    // }
+    // else {
+    //   var html = `<div class="main__messages__message">
+    //                 <div class="main__messages__message__heading">
+    //                   <div class="main__messages__message__heading__name">
+    //                     ${message.name}
+    //                   </div>
+    //                   <div class="main__messages__message__heading__date">
+    //                     ${message.created_at}
+    //                   </div>
+    //                 </div>
+    //                 <div class="main__messages__message__content"></div>
+    //                   <div class="main__messages__message__text">
+    //                     ${message.body}
+    //                   </div>
+    //                 </div>`
+    // }
+
+
+      var has_image = message.image ? 
+                      `<div class="main__messages__message__image">
+                         <image src="${message.image}">
+                      </div>`
+                      : "";
+
+
+
+      var html = `<div class="main__messages__message">
+                    <div class="main__messages__message__heading">
+                      <div class="main__messages__message__heading__name">
+                        ${message.name}
+                      </div>
+                      <div class="main__messages__message__heading__date">
+                        ${message.created_at}
+                      </div>
+                    </div>
+                    <div class="main__messages__message__content"></div>
+                      <div class="main__messages__message__text">
+                        ${message.body}
+                      </div>
+                      ${has_image}
+                    </div>`
+    return html;
+    }
+    
+
+
   $(".new_message").on("submit", function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -9,8 +75,19 @@ $(function(){
       data: formData,
       dataType: "JSON",
       processData: false,
-      contextType: false
+      contentType: false
     })
 
+    .done(function(message){
+      var html = buildHTML(message);
+      $(".main__messages").append(html);
+      $(".new_message")[0].reset();
+      $('.main').animate({ scrollTop: $('.main')[0].scrollHeight});
+      $('.forms__inputForm__btnSend').prop('disabled', false);
+    })
+    
+    .fail(function(){
+      alert("error");
+    })
   })
 });
