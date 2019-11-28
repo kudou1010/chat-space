bbe92d01864f81d151eed7edc91a8a24f21beb9c
kudou_kyohei <kudou.pg1010@gmail.com>
2019-11-28 10:17:18 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,50 +1,11 @@
 $(function(){
   function buildHTML(message) {
-    // if(message.image) {
-    //   var html = `<div class="main__messages__message">
-    //                 <div class="main__messages__message__heading">
-    //                   <div class="main__messages__message__heading__name">
-    //                     ${message.name}
-    //                   </div>
-    //                   <div class="main__messages__message__heading__date">
-    //                     ${message.created_at}
-    //                   </div>
-    //                 </div>
-    //                 <div class="main__messages__message__content"></div>
-    //                   <div class="main__messages__message__text">
-    //                     ${message.body}
-    //                   </div>
-    //                   <div class="main__messages__message__image">
-    //                     ${message.image}
-    //                   </div>
-    //                 </div>`
-    // }
-    // else {
-    //   var html = `<div class="main__messages__message">
-    //                 <div class="main__messages__message__heading">
-    //                   <div class="main__messages__message__heading__name">
-    //                     ${message.name}
-    //                   </div>
-    //                   <div class="main__messages__message__heading__date">
-    //                     ${message.created_at}
-    //                   </div>
-    //                 </div>
-    //                 <div class="main__messages__message__content"></div>
-    //                   <div class="main__messages__message__text">
-    //                     ${message.body}
-    //                   </div>
-    //                 </div>`
-    // }
-
-
       var has_image = message.image ? 
                       `<div class="main__messages__message__image">
                          <image src="${message.image}">
                       </div>`
                       : "";
-
-
-
+                      
       var html = `<div class="main__messages__message">
                     <div class="main__messages__message__heading">
                       <div class="main__messages__message__heading__name">
@@ -62,8 +23,6 @@ $(function(){
                     </div>`
     return html;
     }
-    
-
 
   $(".new_message").on("submit", function(e){
     e.preventDefault();
